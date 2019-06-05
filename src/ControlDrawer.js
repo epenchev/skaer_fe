7b9dfa8f0000fab2562a19d27c7da865d7c03475
Emil Penchev <emo@clearcorrect.com>
2019-06-05 18:15:36 +0300

    Change control positions, restyle main menu

--- src/ControlDrawer.js
+++ src/ControlDrawer.js
@@ -4,7 +4,8 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import AddLibIcon from '@material-ui/icons/LibraryAdd';
+import LibIcon from '@material-ui/icons/LibraryBooks';
 import MailIcon from '@material-ui/icons/Mail';
 import ListItemText from '@material-ui/core/ListItemText';
 import Drawer from '@material-ui/core/Drawer';
@@ -28,27 +29,28 @@ class DrawerList extends Component {
     return (
       <div className={classes.list}>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem className={classes.listItem} button component='a' href="#simple">
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemIcon><AddLibIcon /></ListItemIcon>
+              <ListItemText primary='Add Collection'/>
             </ListItem>
+        </List>
+        <Divider />
+        <List>
+        {[ { title: 'Collections', icon: LibIcon }, 
+            { title: 'Playlists', icon: LibIcon},
+            { title: 'Online Content', icon: LibIcon }
+          ].map((entry, index) => (
+              <ListItem button key={entry.title}>
+                <ListItemIcon><entry.icon /></ListItemIcon>
+                <ListItemText primary={entry.title} />
+              </ListItem>
         ))}
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          {['Settings', 'Help'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-        ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon><MailIcon /></ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
         ))}
--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -6,26 +6,64 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import FilterIcon from '@material-ui/icons/FilterList';
 import CastIcon from '@material-ui/icons/Cast';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+
 
 const styles = theme => ({
   menuButton: {
-    flexGrow: 2.5,
+    flexGrow: 1,
   },
+
   functionButtons: {
     flexGrow: 1,
   },
-  accountButton: {
+
+  buttonsBox: {
     display: 'flex',
-    flexGrow: 2.5,
     justifyContent: 'right',
   },
-  buttonBox: {
+
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
     display: 'flex',
-    justifyContent: 'space-evenly',
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 400,
+    },
+  },
+
 });
 
 
@@ -33,31 +71,39 @@ class ControlPanel extends Component {
   render() {
     const {classes} = this.props;
     return (
-        <AppBar position="fixed">
+        <AppBar position='static'>
           <Toolbar>
             <div className={classes.menuButton}>
               <IconButton onClick={this.props.onMenuButton} edge="start"
                   className={classes.menuButton} color="inherit" aria-label="Menu">
-                <MenuIcon />
+              <MenuIcon />
               </IconButton>
+            </div>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon/>
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'Search' }}
+              />
             </div>
             <div className={classes.functionButtons}>
-              <div className={classes.buttonBox} >
-              <IconButton color="inherit">
-                <SearchIcon />
-              </IconButton>
-              <IconButton color="inherit">
-                <MoreIcon  />
-              </IconButton>
-              <IconButton color="inherit">
-                <CastIcon />
-              </IconButton>
+              <div className={classes.buttonsBox}>
+                <IconButton color="inherit">
+                  <FilterIcon/>
+                </IconButton>
+                <IconButton color="inherit">
+                  <CastIcon />
+                </IconButton>
+                <IconButton color="inherit">
+                  <AccountCircle />
+                </IconButton>
               </div>
-            </div>
-            <div className={classes.accountButton}>
-              <IconButton color="inherit">
-                <AccountCircle />
-              </IconButton>
             </div>
           </Toolbar>
        </AppBar>
